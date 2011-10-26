02ce12bbda4a15f85c31816249693d157039c660
Brendan Loudermilk <brendan@apwit.com>
2011-10-26 00:45:52 -0700

    Implemented the rest of the BaseExtensions spec

--- spec/acts_as_async/base_extensions_spec.rb
+++ spec/acts_as_async/base_extensions_spec.rb
@@ -16,22 +16,39 @@ describe ActsAsAsync::BaseExtensions do
     end
 
     context "when @queue is already set" do
+      let(:model) do
+        BareModel.instance_variable_set(:@queue, :old_queue)
+        BareModel
+      end
+
       context "when :queue is passed as an option" do
-        it "should override the @queue value to the value passed"
+        it "should override the @queue value to the value passed" do
+          model.acts_as_async :queue => :new_queue
+          model.instance_variable_get(:@queue).should == :new_queue
+        end
       end
 
       context "when :queue isn't passed as an option" do
-        it "should leave the existing @queue value alone"
+        it "should leave the existing @queue value alone" do
+          model.acts_as_async
+          model.instance_variable_get(:@queue).should == :old_queue
+        end
       end
     end
 
     context "when @queue isn't set" do
       context "when :queue is passed as an option" do
-        it "should set @queue to the value passed"
+        it "should set @queue to the value passed" do
+          BareModel.acts_as_async :queue => :foobar
+          BareModel.instance_variable_get(:@queue).should == :foobar
+        end
       end
 
       context "when :queue isn't passed as an option" do
-        it "should set @queue to :default"
+        it "should set @queue to :default" do
+          BareModel.acts_as_async
+          BareModel.instance_variable_get(:@queue).should == :default
+        end
       end
     end
   end
