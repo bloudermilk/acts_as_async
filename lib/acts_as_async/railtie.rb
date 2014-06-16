413002313690c2ca7ffae45c2e75c081acefacd7
Lukasz Tackowiak <lukasz.tackowiak@polcode.com>
2014-06-16 13:14:39 +0200

    Fix require for Resque Scheduler tasks also.

--- lib/acts_as_async/railtie.rb
+++ lib/acts_as_async/railtie.rb
@@ -2,7 +2,11 @@ module ActsAsAsync
   class Railtie < Rails::Railtie
     rake_tasks do
       require "resque/tasks"
-      require "resque_scheduler/tasks"
+      begin
+        require "resque_scheduler/tasks"
+      rescue LoadError
+        require "resque/scheduler/tasks"
+      end
 
       task "resque:setup" => :environment
     end
